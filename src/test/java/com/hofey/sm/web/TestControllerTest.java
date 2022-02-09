7f80e64da7fc0262f95798b386d9daea6f6158fb
hofey <hofey@live.com>
2022-02-09 18:21:24 +0800

    springboot mybatis

--- src/test/java/com/hofey/sm/web/TestControllerTest.java
+++ src/test/java/com/hofey/sm/web/TestControllerTest.java
@@ -2,25 +2,24 @@ package com.hofey.sm.web;
 
 import com.hofey.sm.entity.User;
 import com.hofey.sm.mapper.UserMapper;
+import com.hofey.sm.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * @Author: hofey
- * @Date: 2022/2/9 11:17
- * @Description:
- */
+
+@SpringBootTest
 class TestControllerTest {
 
     @Autowired
-    private UserMapper userMapper;
+    private UserService userService;
 
     @Test
     void selectUser() {
 
-        User user = userMapper.selectUser(1);
+        User user = userService.getUser(1);
         System.out.println(user.toString());
     }
 }
